6c6cd54b8db9f413a8458ef684151fd4d4885ff3
이준기 <jg0411@naver.com>
2022-05-17 04:13:08 +0900

    ProcMon의 기능을 테스트할 ProcMonTest 프로그램 추가

---
+++ ProcMonTest/FileTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ProcMonTest
+{
+    public class FileTest
+    {
+        List<FileStream> FileList;
+
+        public FileTest()
+        {
+            FileList = new List<FileStream>();
+        }
+
+        public void PrintList()
+        {
+            for (int i = 0; i < FileList.Count; i++)
+            {
+                Console.WriteLine("\t" + i + " : " + FileList[i].Name);
+            }
+        }
+
+        public void OpenFile()
+        {
+            string input;
+            string path;
+            FileMode mode;
+            FileAccess access;
+
+            try
+            {
+                Console.WriteLine("\tOpenFile");
+                Console.Write("\tPath\n> ");
+                path = Console.ReadLine();
+
+                Console.Write("\tFileMode\n\t1 : CreateNew\n\t2 : Create \n\t3 : Open\n\t4 : OpenOrCreate\n\t5 : Truncate\n\t6 : Append\n> ");
+                input = Console.ReadLine();
+                mode = (FileMode)int.Parse(input);
+
+                Console.Write("\tFileAccess\n\t1 : Read\n\t2 : Write\n\t3: ReadWrite\n> ");
+                input = Console.ReadLine();
+                access = (FileAccess)int.Parse(input);
+
+                var file = File.Open(path, mode, access);
+                FileList.Add(file);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void CloseFile()
+        {
+            try
+            {
+                string input;
+                int num;
+                Console.Write("\tClose File Index : ");
+                input = Console.ReadLine();
+                num = int.Parse(input);
+
+                FileList[num].Close();
+                FileList.RemoveAt(num);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void DeleteFile()
+        {
+            try
+            {
+                string input;
+                Console.Write("\tDelete File Path : ");
+                input = Console.ReadLine();
+                File.Delete(input);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+
+    public class RegistryTest
+    {
+
+    }
+}
---
+++ ProcMonTest/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace ProcMonTest
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Win32.AllocConsole();
+
+            new Thread(new ThreadStart(threadcontext)).Start();
+        }
+
+        void threadcontext()
+        {
+            var f = new FileTest();
+
+            while (true)
+            {
+                try
+                {
+                    string input;
+                    int num;
+                    Console.Write("File\n1 : Print List\n2 : Open File\n 3 : Close File\n4 : Delete File\n> ");
+                    input = Console.ReadLine();
+                    num = int.Parse(input);
+
+                    switch (num)
+                    {
+                        case 1:
+                            f.PrintList();
+                            break;
+                        case 2:
+                            f.OpenFile();
+                            break;
+                        case 3:
+                            f.CloseFile();
+                            break;
+                        case 4:
+                            f.DeleteFile();
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+
+        public class Win32
+        {
+            [DllImport("kernel32.dll")]
+            public static extern Boolean AllocConsole();
+            [DllImport("kernel32.dll")]
+            public static extern Boolean FreeConsole();
+        }
+
+    }
+}
